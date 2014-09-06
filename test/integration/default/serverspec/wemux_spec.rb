bd7896b28403d40cb291cb9c66e16b250e9c87af
James Cuzella <james.cuzella@lyraphase.com>
2014-09-05 18:22:56 -0600

    Adding serverspec tests and wrapper playbook to test wemux role

---
+++ test/integration/default/serverspec/wemux_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe file('/usr/local/share/wemux/wemux') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file('/usr/local/bin/wemux') do
+  it { should be_file }
+  it { should be_executable }
+  it { should be_executable.by('others') }
+  it { should be_linked_to '/usr/local/share/wemux/wemux' }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file('/usr/local/etc/wemux.conf') do
+  it { should be_file }
+  its(:content) { should match /^host_list=\(\s*localhost\s*\)/ }
+end
