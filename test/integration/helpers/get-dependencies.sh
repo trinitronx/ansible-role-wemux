7fccf59cad7db5f1daeea9db396f37cbefb3d5af
James Cuzella <james.cuzella@lyraphase.com>
2018-02-03 13:12:13 -0700

    Fix canonicalize_path_cmd to be platform independent (sorta)

--- test/integration/helpers/get-dependencies.sh
+++ test/integration/helpers/get-dependencies.sh
@@ -6,8 +6,13 @@ umask 022
 if [ $# != 0 ]; then
 testhelpersdir=$1
 else
-testhelpersdir="$(dirname $0)/../../../.."
-testhelpersdir="$(realpath "$testhelpersdir")"
+  testhelpersdir="$(dirname $0)/../../../.."
+  if [ -x "$(which realpath)" ]; then
+    canonicalize_path_cmd='realpath'
+  elif [ -x "$(which readlink)" ]; then
+    canonicalize_path_cmd='readlink -f'
+  fi
+  testhelpersdir="$($canonicalize_path_cmd "$testhelpersdir")"
 fi
 
 #[ ! -d $testhelpersdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $testhelpersdir/juju4.redhat-epel
